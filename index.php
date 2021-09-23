092847a0df3d78375f9b0fc4938536b1cf2bccdf
Katia Sheleh <kate.sheleh@gmail.com>
2021-09-23 21:03:41 +0200

    code formatting

--- index.php
+++ index.php
@@ -3,44 +3,44 @@ $isAuth = rand(0, 1);
 
 $userName = 'Katia Sheleh';
 
-$categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
 
-$ads = [
+$lots = [
     [
-        "name" => "2014 Rossignol District Snowboard",
-        "category" => "Доски и лыжи",
-        "price" => "10999",
-        "imgUrl" => "img/lot-1.jpg"
+        'name' => '2014 Rossignol District Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => '10999',
+        'imgUrl' => 'img/lot-1.jpg'
     ],
     [
-        "name" => "DC Ply Mens 2016/2017 Snowboard",
-        "category" => "Доски и лыжи",
-        "price" => "159999",
-        "imgUrl" => "img/lot-2.jpg"
+        'name' => 'DC Ply Mens 2016/2017 Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => '159999',
+        'imgUrl' => 'img/lot-2.jpg'
     ],
     [
-        "name" => "Крепления Union Contact Pro 2015 года размер L/XL",
-        "category" => "Крепления",
-        "price" => "8000",
-        "imgUrl" => "img/lot-3.jpg"
+        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'category' => 'Крепления',
+        'price' => '8000',
+        'imgUrl' => 'img/lot-3.jpg'
     ],
     [
-        "name" => "Ботинки для сноуборда DC Mutiny Charocal",
-        "category" => "Ботинки",
-        "price" => "10999",
-        "imgUrl" => "img/lot-4.jpg"
+        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'category' => 'Ботинки',
+        'price' => '10999',
+        'imgUrl' => 'img/lot-4.jpg'
     ],
     [
-        "name" => "Куртка для сноуборда DC Mutiny Charocal",
-        "category" => "Одежда",
-        "price" => "7500",
-        "imgUrl" => "img/lot-5.jpg"
+        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'category' => 'Одежда',
+        'price' => '7500',
+        'imgUrl' => 'img/lot-5.jpg'
     ],
     [
-        "name" => "Маска Oakley Canopy",
-        "category" => "Разное",
-        "price" => "5400",
-        "imgUrl" => "img/lot-6.jpg"
+        'name' => 'Маска Oakley Canopy',
+        'category' => 'Разное',
+        'price' => '5400',
+        'imgUrl' => 'img/lot-6.jpg'
     ]
     ];
 ?>
@@ -72,7 +72,7 @@ $ads = [
         <!-- show user info if $isAuth is true -->
         <?php if($isAuth === 1) : ?>
             <div class="user-menu__logged">
-                <p><?= $userName; ?></p>
+                <p><?= $userName ?></p>
                 <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                 <a class="user-menu__logout" href="#">Выход</a>
             </div>
@@ -100,7 +100,9 @@ $ads = [
             <!-- Show lots categories -->
             <?php foreach ($categories as $category): ?>
                 <li class="promo__item promo__item--boards">
-                    <a class="promo__link" href="pages/all-lots.html"><?=$category;?></a>
+                    <a class="promo__link" href="pages/all-lots.html">
+                        <?= $category ?>
+                    </a>
                 </li>
             <?php endforeach; ?>
         </ul>
@@ -111,18 +113,22 @@ $ads = [
         </div>
         <ul class="lots__list">
             <!-- Lots list -->
-            <?php foreach ($ads as $adKey => $adValue): ?>
+            <?php foreach ($lots as $lotKey => $lotValue): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$adValue['imgUrl']; ?>" width="350" height="260" alt="">
+                    <img src="<?= $lotValue['imgUrl'] ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$adValue["category"]; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$adValue["name"]; ?></a></h3>
+                    <span class="lot__category">
+                        <?= $lotValue['category'] ?>
+                    </span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $lotValue['name'] ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$adValue["price"]; ?><b class="rub">р</b></span>
+                            <span class="lot__cost">
+                                <?= $lotValue['price'] ?><b class="rub">р</b>
+                            </span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -142,7 +148,9 @@ $ads = [
             <!-- Show lots categories -->
             <?php foreach ($categories as $category): ?>
                 <li class="nav__item">
-                    <a href="pages/all-lots.html"><?=$category;?></a>
+                    <a href="pages/all-lots.html">
+                        <?= $category ?>
+                    </a>
                 </li>
             <?php endforeach; ?>
         </ul>
