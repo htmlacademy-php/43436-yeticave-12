450d94111c5daa0696a9a516fabb2c5507df7c43
Katia Sheleh <kate.sheleh@gmail.com>
2021-09-30 15:18:30 +0200

    fix errors

--- templates/schema.sql
+++ templates/schema.sql
@@ -1,45 +1,46 @@
-CREATE DATABASE auctionshop
+CREATE DATABASE IF NOT EXISTS auctionshop
 DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
 
 use auctionshop;
 
 CREATE TABLE categories(
-    `id` INT PRIMARY KEY,
+    `id` VARCHAR(32) DEFAULT (uuid()) PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL,
     `technical_name` VARCHAR(64) NOT NULL
 );
 
 CREATE TABLE users(
-    `id` INT PRIMARY KEY,
-    `registrated_at` DATE DEFAULT CURRENT_DATE,
-    `email` VARCHAR(255) NOT NULL,
+    `id` VARCHAR(32) DEFAULT (uuid()) PRIMARY KEY,
+    `registered_at` DATE,
+    `email` VARCHAR(255) UNIQUE NOT NULL,
     `password` VARCHAR(255) NOT NULL,
-    `phone` VARCHAR(255) NOT NULL
+    `phone` VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE lots(
-    `id` INT PRIMARY KEY,
-    `created_at` DATE DEFAULT CURRENT_DATE,
+    `id` VARCHAR(32) DEFAULT (uuid()) PRIMARY KEY,
+    `created_at` DATE,
     `name` VARCHAR(255) NOT NULL,
-    `description` TEXT,
-    `start_price` INT UNSIGNED DEFAULT 0,
+    `description` TEXT NOT NULL,
+    `image_url` VARCHAR(255) NOT NULL,
+    `start_price` INT UNSIGNED DEFAULT 0 NOT NULL,
     `expiration_at` DATE NOT NULL,
     `rate_step` INT UNSIGNED NOT NULL,
-    `category_id_FK` INT,
-    `author_id_FK` INT,
-    `winner_id_FK` INT,
-    FOREIGN KEY (`category_id_FK`) REFERENCES categories(`id`),
-    FOREIGN KEY (`author_id_FK`) REFERENCES users(`id`),
-    FOREIGN KEY (`winner_id_FK`) REFERENCES users(`id`)
+    `category_id` VARCHAR(32) NOT NULL,
+    `author_id` VARCHAR(32) NOT NULL,
+    `winner_id` VARCHAR(32) NOT NULL,
+    FOREIGN KEY (`category_id`) REFERENCES categories(`id`),
+    FOREIGN KEY (`author_id`) REFERENCES users(`id`),
+    FOREIGN KEY (`winner_id`) REFERENCES users(`id`)
 );
 
 CREATE TABLE rates(
-    `id` INT PRIMARY KEY,
-    `created_at` DATE DEFAULT CURRENT_DATE,
-    `price` INT UNSIGNED DEFAULT 0,
-    `user_id_FK` INT,
-    `lot_id_FK` INT,
-    FOREIGN KEY (`user_id_FK`) REFERENCES users(`id`),
-    FOREIGN KEY (`lot_id_FK`) REFERENCES lots(`id`)
+    `id` VARCHAR(32) DEFAULT (uuid()) PRIMARY KEY,
+    `created_at` DATE,
+    `price` INT UNSIGNED DEFAULT 0 NOT NULL,
+    `user_id` VARCHAR(32) NOT NULL,
+    `lot_id` VARCHAR(32) NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES users(`id`),
+    FOREIGN KEY (`lot_id`) REFERENCES lots(`id`)
 );
